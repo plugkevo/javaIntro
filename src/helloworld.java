6fefac3ebc5dfa948377430a73021416e095815f
plugkevo <munenekevin366@gmail.com>
2024-10-03 12:33:58 +0300

    Variables

---
+++ src/helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+    public static void main (String[]args) {
+        System.out.println("hello");
+
+
+        var i=5;
+        for (i=0; i<=10; i++);
+        System.out.println(i);
+
+        }
+    }
+
---
+++ src/variables.java
@@ -0,0 +1,9 @@
+public class variables {
+    public static void main (String[]args) {
+        var num1 = 5;
+        var num2=6;
+
+         System.out.println(num1);
+        System.out.println(num2);
+    }
+}
