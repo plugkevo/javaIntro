45cb43e233ec0c41c59447531eea33e2318c764d
plugkevo <munenekevin366@gmail.com>
2024-10-31 21:13:10 +0300

    ass1

---
+++ src/thistest/ass1.java
@@ -0,0 +1,12 @@
+package thistest;
+
+public class ass1 {
+    public static void main(String[] args) {
+        System.out.println(fun());
+    }
+
+    static int fun()
+    {
+        return 20;
+    }
+}
